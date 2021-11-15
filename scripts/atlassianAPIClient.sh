0b1ca0ec736e00a8e273e677b5a46a118678ee90
tripattern <systems@tripattern.com>
2021-11-15 16:06:17 +0100

    refactored atlassian bash script

---
+++ scripts/atlassianAPIClient.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# $1 path to file with access info e.g. ./atlassianAPIClient.sh ~/path/filename.conf
+cd ..
+echo "CHECK! Are you in the pipenv shell?"
+# pipenv shell
+python atlassian_api_client $1
