2af2da7f8ce78ef77d057fd8238700727d024939
tripattern <systems@tripattern.com>
2021-11-18 14:10:01 +0100

    created atlassian api client module in atlassian lab

--- atlassian_lab/atlassian_api_client/__main__.py
+++ atlassian_lab/atlassian_api_client/__main__.py
@@ -1,7 +1,7 @@
 import sys
 
 import configparser
-import atlassian_lab
+import atlassian_api_client
 
 # Config file format is like an .ini file
 # [atlassian_api_client]
@@ -13,8 +13,8 @@ def main():
     user = config['atlassian_api_client']['user']
     api_token = config['atlassian_api_client']['api_token']
     server = config['atlassian_api_client']['server']
-    jira = atlassian_lab.connect_to_jira_api(api_token, server, user)
-    atlassian_lab.get_pbi_data(jira, config['atlassian_api_client']['issue'])
+    jira = atlassian_api_client.connect_to_jira_api(api_token, server, user)
+    atlassian_api_client.get_pbi_data(jira, config['atlassian_api_client']['issue'])
 
 
 if __name__ == '__main__':
--- scripts/atlassianAPIClient.sh
+++ scripts/atlassianAPIClient.sh
@@ -3,4 +3,4 @@
 cd ..
 echo "CHECK! Are you in the pipenv shell?"
 # pipenv shell
-python atlassian_api_client $1
+python atlassian_lab/atlassian_api_client $1
