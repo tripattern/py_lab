46d920c07485536e573e845398922a9ba9bdfd6a
tripattern <systems@tripattern.com>
2021-12-10 18:28:47 +0100

    comment fix

--- fp/func_prog_examples.py
+++ fp/func_prog_examples.py
@@ -4,5 +4,5 @@ print(wn_squared)
 
 tuple_natural_numbers = (1, 2, 3, 4, 5, 6, 7)  # tuples are ordered, unchangeable
 tuple_nn_squared = tuple(map(lambda x: x ** 2, tuple_natural_numbers))
-# tuple_nn_squared[0] = 5 # this should not work as lists are immutable
+# tuple_nn_squared[0] = 5 # this should not work as tuples are immutable
 print(tuple_nn_squared)
