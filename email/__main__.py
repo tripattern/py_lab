274d3ba9e77aea25075606c0a6c48ab3a8ffd531
tripattern <systems@tripattern.com>
2021-12-17 16:19:11 +0100

    simple example of mailing with sendgrid

--- email/__main__.py
+++ email/__main__.py
@@ -1,16 +1,19 @@
 import sys
-import email_mailjet
+import configparser
+import email_sendgrid
+
 
 def main():
-    api_key = sys.argv[1]
-    api_secret = sys.argv[2]
-    to_address = sys.argv[3]
-    from_address = sys.argv[4]
-    custom_id = "NewsGathered"
+    config = configparser.ConfigParser()
+    config.read(sys.argv[1])
     subject = "News you can use, maybe..."
-    text_message = "Did you disable html emails?"
     html_message = "<h2>Hi, here is your news for the day! Can you use news from <a href='https://bbc.com/'>BBC</a></h2><br />Have a great day!"
-    email_mailjet.send_email(api_key, api_secret, to_address, from_address, custom_id, subject, text_message, html_message)
+    email_sendgrid.send_email(
+        config['email_sendgrid']['from_email'],
+        config['email_sendgrid']['to_emails'],
+        config['email_sendgrid']['api_key'],
+        subject,
+        html_message)
 
 
 if __name__ == '__main__':
---
+++ email/email_sendgrid.py
@@ -0,0 +1,16 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+# os.environ.get('SENDGRID_API_KEY') # could also use environment variables to access the API key
+
+def send_email(str_from_email, str_to_emails, str_api_key, str_subject, str_html_content):
+    mail_message = Mail(
+        from_email=str_from_email,
+        to_emails=str_to_emails,
+        subject=str_subject,
+        html_content=str_html_content)
+    sg = SendGridAPIClient(str_api_key)
+    response = sg.send(mail_message)
+    print(response.status_code, response.body, response.headers)
