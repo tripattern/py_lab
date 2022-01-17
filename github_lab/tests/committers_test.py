7f18731b254b06942bdf8fcbd11948bcedfe0282
tripattern <systems@tripattern.com>
2022-01-17 10:08:54 +0100

    updated committers query

--- github_lab/tests/committers_test.py
+++ github_lab/tests/committers_test.py
@@ -11,12 +11,12 @@ def test_it_should_get_committers():
     str_user: str = config['gh_tri_api']['user']
     str_repo: str = 'py_lab'
     str_api_token: str = config['gh_tri_api']['api_token']
-    str_url_api_commits: str = "https://api.github.com/repos/{}/{}/commits?branch=master&since=2021-12-03&until=2021-12-10".format(str_user, str_repo)
+    str_url_api_commits: str = "https://api.github.com/repos/{}/{}/commits?branch=master&since=2021-12-01&until=2021-12-31".format(str_user, str_repo)
     print(str_url_api_commits)
     req_gh_session: requests = requests.Session()
     req_gh_session.auth = (str_user, str_api_token)
     response: list = json.loads(req_gh_session.get(str_url_api_commits).text)
-    print(response)
+    print(response[0])
     assert 1 == 1
 
 # pytest ./github_lab/tests/committers_test.py::test_it_should_get_committers -rA
